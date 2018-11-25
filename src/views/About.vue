9004787a7a996eee53854246571358523339e070
Andrew Palmer <rafal.dardzinski@gmail.com>
2018-11-26 00:36:50 +0100

    added chart components for testing purposes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,44 @@
 <template>
   <div class="about">
-    <overviewer-chart></overviewer-chart>
+    <overvue-error-boundary>
+      <overvue-chart 
+      chart-type="bar"
+      :loaded="dataLoaded"
+      :datasets="[{label: 'First', data: [2, 5, 8]},
+      {label: 'Second', data: [10, 11, 3]}]"
+      :labels="['One', 'Two', 'Three']"
+      />
+      <overvue-chart 
+      chart-type="line"
+      :loaded="dataLoaded"
+      :datasets="[{label: 'First', data: [0, 1, 2]},
+      {label: 'Second', data: [1, 8, 5]}]"
+      :labels="['One', 'Two', 'Three']"
+      />
+    </overvue-error-boundary>
   </div>
 </template>
 <script>
-import Chart from '@/components/chart/chart.vue';
+import OvervueChart from '@/components/chart/chart.vue';
+import OvervueErrorBoundary from '@/components/error-boundary.vue';
+
 export default {
   components: {
-    'overviewer-chart': Chart
+    'overvue-chart': OvervueChart,
+    'overvue-error-boundary': OvervueErrorBoundary
+  },
+  data() {
+    return {
+      dataLoaded: false
+    }
+  },
+  methods: {
+    setDataLoaded() {
+      this.dataLoaded = true;
+    }
+  },
+  mounted() {
+    setTimeout(this.setDataLoaded, 2000);
   }
 }
 </script>
