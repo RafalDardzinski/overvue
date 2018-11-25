67c083836826c7f2f201e82c4b4f7a4a5b080b94
Andrew Palmer <rafal.dardzinski@gmail.com>
2018-11-25 22:34:56 +0100

    added: LoadingIndicator component

---
+++ src/components/loading-indicator.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="overvue-loading-indicator">
+    <!-- @todo: add animated icons -->
+    Loading...
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss" scoped>
+.overdue-loading-indicator {
+  text-align: center;
+}
+</style>
