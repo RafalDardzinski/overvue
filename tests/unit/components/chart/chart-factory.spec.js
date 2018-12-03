51c8c60512a7fb70e5fdb660740bad895328a8cc
Rafał Dardziński <rafal.dardzinski@gmail.com>
2018-12-03 17:54:23 +0100

    added: chart-factory unit tests

---
+++ tests/unit/components/chart/chart-factory.spec.js
@@ -0,0 +1,24 @@
+import {expect} from 'chai';
+
+import ChartFactory from '@/components/chart/chart-factory';
+import BarChart from '@/components/chart/bar-chart';
+import LineChart from '@/components/chart/line-chart';
+
+describe('ChartFactory (@/components/chart/chart-factory.js)', () => {
+  let canvasRef;
+  beforeEach(() => {
+    canvasRef = document.createElement('canvas');
+  });
+
+  describe('static methods', () => {
+    describe('createEmptyChartInstance()', () => {
+      it('returns BarChart instance when called with "bar" as type argument', () => {
+        expect(ChartFactory.createEmptyChartInstance(canvasRef, 'bar')).to.be.an.instanceOf(BarChart);
+      });
+
+      it('returns LineChart instance when called with "line" as type argument', () => {
+        expect(ChartFactory.createEmptyChartInstance(canvasRef, 'line')).to.be.an.instanceOf(LineChart);
+      });
+    });
+  });
+});
