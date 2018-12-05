b8f36e864e4fe56754f952c6dbbd2b3ef30beff9
Rafał Dardziński <rafal.dardzinski@gmail.com>
2018-12-05 20:53:55 +0100

    added: LineChart.styleDatasets which adds specific stylings to line charts

--- src/components/chart/line-chart.js
+++ src/components/chart/line-chart.js
@@ -6,6 +6,14 @@ class LineChart extends Chart {
   constructor(canvasRef, data, opts = config.options[type]) {
     super(canvasRef, type, data, opts);
   }
+
+  static styleDatasets(datasets, fillMode = false) {
+    return Chart.styleDatasets(datasets, config.datasets.line)
+      .map(dataset => {
+        dataset.fill = fillMode;
+        return dataset;
+      });
+  }
 }
 
 export default LineChart;
--- tests/unit/components/chart/line-chart.js.spec.js
+++ tests/unit/components/chart/line-chart.js.spec.js
@@ -1,13 +1,18 @@
 import chai from 'chai';
 import chaiSubset from 'chai-subset';
+import spies from 'chai-spies';
 import { expect } from 'chai';
 import Chart from '@/components/chart/chart';
 import LineChart from '@/components/chart/line-chart';
 import ChartJS from 'chart.js';
 import config from '@/components/chart/config';
+import utils from './utils';
+
 
 chai.use(chaiSubset);
+chai.use(spies);
 
+const { mockDatasets } = utils;
 const mockChartCreateParams = ( data = {}, options = {}) => {
   const canvasRef = document.createElement('canvas');
   canvasRef.getContext = () => {return {};};
@@ -47,6 +52,37 @@ describe('LineChart (@/components/chart/line-chart.js)', () => {
         const {canvasRef, data} = mockChartCreateParams();
         const lineChart = new LineChart(canvasRef, data);
         expect(lineChart.ref.options).to.containSubset(config.options.line || config.options.default);
+      });
+    });
+  });
+
+  describe('LineChart.styleDatasets(datasets, fillMode)', () => {
+    let mockedDatasets;
+
+    beforeEach(() => {
+      mockedDatasets = mockDatasets();
+    });
+
+    it('calls Chart.styleDatasets with config.datasets.line as configuration argument', () => {
+      const spy = chai.spy.on(Chart, 'styleDatasets');
+      LineChart.styleDatasets(mockedDatasets);
+      expect(spy).to.have.been.called.with(config.datasets.line);
+    });
+
+    it('returns datasets, each with fill property equal to value passed as fillMode argument', () => {
+      const fillMode = 'origin';
+      const styledDatasets = LineChart.styleDatasets(mockedDatasets, fillMode);
+      styledDatasets.forEach(dataset => {
+        expect(dataset).to.have.property('fill').that.equals(fillMode);
+      });
+    });
+
+    describe('when fillMode is not defined', () => {
+      it('returns datasets, each with fill: false', () => {
+        const styledDatasets = LineChart.styleDatasets(mockedDatasets);
+        styledDatasets.forEach(dataset => {
+          expect(dataset).to.have.property('fill').that.equals(false);
+        });
       });
     });
   });
