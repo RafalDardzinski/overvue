1d26523e5ab1419a3905df47745143cab9840c2c
Andrew Palmer <rafal.dardzinski@gmail.com>
2018-12-14 16:25:34 +0100

    button styles

--- src/views/Buttons.vue
+++ src/views/Buttons.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="buttons">
-    <header><h4>Normal</h4></header>
-    <button>Template</button>
-    <button class="button-primary">Primary</button>
-    <button class="button-secondary">Secondary</button>
-    <button class="button-success">Success</button>
-    <button class="button-warning">Warning</button>
-    <button class="button-danger">Danger</button>
-    <button class="button-info">Info</button>
-    <button class="button-light">Light</button>
-    <button class="button-dark">Dark</button>
-    <button class="button-link">Link</button> 
+    <section>
+      <header><h4>Normal</h4></header>
+      <button class="button-primary">Primary</button>
+      <button class="button-secondary">Secondary</button>
+      <button class="button-success">Success</button>
+      <button class="button-warning">Warning</button>
+      <button class="button-danger">Danger</button>
+      <button class="button-info">Info</button>
+    </section>
+    <section>
+      <header><h4>Disabled</h4></header>
+      <button class="button-primary button--disabled">Primary</button>
+      <button class="button-secondary button--disabled">Secondary</button>
+      <button class="button-success button--disabled">Success</button>
+      <button class="button-warning button--disabled">Warning</button>
+      <button class="button-danger button--disabled">Danger</button>
+      <button class="button-info button--disabled">Info</button>
+    </section>
   </div>
 </template>
 
@@ -29,13 +36,13 @@ export default {
 .buttons {
   margin: 1rem 0;
 
-  &>button {
+  & button {
     margin: .5rem;
     width: 100px;
   }
 
   & h4 {
-    margin: 0;
+    margin: 0 .5rem;
   }
 
 }
