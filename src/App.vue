edf5a3e506228413c328b43cd669d4a92374aed7
Andrew Palmer <rafal.dardzinski@gmail.com>
2018-12-17 02:32:43 +0100

    implemented: reactive window width tracking

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" ref="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> | 
@@ -8,16 +8,51 @@
     <router-view/>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      resizeTimeout: null
+    }
+  },
+  methods: {
+    getAppOffsetWidth() {
+      return this.$refs.app.offsetWidth;
+    },
+    commitAppOffsetWidth(offsetWidth) {
+      this.$store.commit('setAppOffsetWidth', offsetWidth);
+    },
+    addWindowResizeListener() {
+      window.addEventListener('resize', this.handleResize);
+      return window;
+    },
+    handleResize() {
+      // promise to make it testable. Timeout to prevent throttling.
+      return new Promise((resolve) => {
+        if(!this.resizeTimeout) {
+          this.resizeTimeout = setTimeout(() => {
+            this.resizeTimeout = null;
+            this.commitAppOffsetWidth(this.getAppOffsetWidth());
+            resolve(true);
+          }, 500);
+        } else {
+          resolve(false)
+        }
+      })
+    }
+  },
+  mounted() {
+    this.commitAppOffsetWidth(this.getAppOffsetWidth());
+    this.addWindowResizeListener();
+  }
+}
+</script>
 
 <style lang="scss">
 @import '@/styles/typography.scss';
 @import '@/styles/buttons.scss';
 
 #app {
-  // font-family: 'Avant Garde SAP', Helvetica, Arial, sans-serif;
-  // -webkit-font-smoothing: antialiased;
-  // -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
   color: #2c3e50;
 }
 
---
+++ tests/unit/App.vue.spec.js
@@ -0,0 +1,148 @@
+import chai from 'chai';
+import spies from 'chai-spies';
+import Vuex from 'vuex';
+import { expect } from 'chai';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import App from '@/App.vue';
+
+chai.use(spies);
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('App.vue (@/App.vue)', () => {
+  describe('methods', () => {
+    describe('getAppOffsetWidth()', () => {
+      it('returns this.$refs.app.offsetWidth', () => {
+        const localThis = {
+          $refs: {
+            app: { offsetWidth: 480 }
+          }
+        };
+
+        expect(App.methods.getAppOffsetWidth.call(localThis))
+          .to.equal(localThis.$refs.app.offsetWidth);
+      });
+    });
+
+    describe('commitAppOffsetWidth(offsetWidth)', () => {
+      let mutations, store, spy;
+
+      beforeEach(() => {
+        const mockFunc = function () { return true; };
+        spy = chai.spy(mockFunc);
+        mutations = { setAppOffsetWidth: spy };
+        store = new Vuex.Store({
+          mutations,
+          state: {
+            APP_OFFSET_WIDTH: 0
+          },
+        });
+      });
+
+      it('commits offsetWidth to store', () => {
+        const wrapper = shallowMount(App, {
+          store,
+          localVue
+        });
+
+        const mockWidth = 460;
+        wrapper.vm.commitAppOffsetWidth(mockWidth);
+        expect(spy).to.have.been.called.with(mockWidth);
+      });
+    });
+
+    describe('addWindowResizeListener()', () => {
+      it('adds "resize" event listener to window object', () => {
+        const spy = chai.spy.on(window, 'addEventListener');
+        const localThis = {
+          handleResize: () => true
+        };
+        App.methods.addWindowResizeListener.call(localThis);
+        expect(spy).to.have.been.called.with.exactly('resize', localThis.handleResize);
+      });
+
+      it('returns window object', () => {
+        const localThis = {
+          handleResize: () => true
+        };
+        const result = App.methods.addWindowResizeListener.call(localThis);
+        expect(result).to.equal(window);
+      });
+    });
+
+    describe('handleResize()', () => {
+      let localThis;
+      describe('when this.resizeTimeout is falsy', () => {
+        beforeEach(() => {
+          localThis = {
+            resizeTimeout: null,
+            commitAppOffsetWidth: () => true,
+            getAppOffsetWidth: () => 458
+          };
+        });
+
+        it('returns true', done => {
+          App.methods.handleResize.call(localThis)
+            .then(result => {
+              expect(result).to.be.true;
+              done();
+            })
+            .catch(error => done(error));
+        });
+
+        it('calls this.commitAppOffsetWidth() with this.getAppOffsetWidth() as parameter', done => {
+          const commitAppOffsetWidthSpy = chai.spy.on(localThis, 'commitAppOffsetWidth');
+
+          App.methods.handleResize.call(localThis)
+            .then(result => {
+              console.log(localThis.resizeTimeout);
+              expect(commitAppOffsetWidthSpy).to.have.been.called.with(localThis.getAppOffsetWidth());
+              done();
+            })
+            .catch(error => done(error));
+        });
+      });
+
+      describe('when !!this.resizeTimeout is true', () => {
+        beforeEach(() => {
+          localThis = {
+            resizeTimeout: 45,
+            commitAppOffsetWidth: () => true,
+            getAppOffsetWidth: () => 458
+          };
+        });
+
+        it('returns false', done => {
+          App.methods.handleResize.call(localThis)
+            .then(result => {
+              expect(result).to.be.false;
+              done();
+            })
+            .catch(error => done(error));
+        });
+
+        it('does not call commitAppOffsetWidth()', done => {
+          const commitAppOffsetWidthSpy = chai.spy.on(localThis, 'commitAppOffsetWidth');
+
+          App.methods.handleResize.call(localThis)
+            .then(result => {
+              expect(commitAppOffsetWidthSpy).to.not.have.been.called();
+              done();
+            })
+            .catch(error => done(error));
+        });
+
+        it('does not modify this.resizeTimeout', done => {
+          const oldResizeTimeoutVal = localThis.resizeTimeout;
+          App.methods.handleResize.call(localThis)
+            .then(result => {
+              expect(localThis.resizeTimeout).to.equal(oldResizeTimeoutVal);
+              done();
+            })
+            .catch(error => done(error));
+        });
+      });
+    });
+  });  
+});
