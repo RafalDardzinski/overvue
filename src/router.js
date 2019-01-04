5f31b7596b409d3843a2de3681a427949a6ee9fb
Andrew Palmer <rafal.dardzinski@gmail.com>
2019-01-04 20:31:05 +0100

    removed component imports

--- src/router.js
+++ src/router.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-import Buttons from './views/Buttons.vue';
 import routes from '@/config/routes';
 
 Vue.use(Router);
