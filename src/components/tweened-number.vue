31e48e1442e3010411c5f7359563202b33a83550
Rafał Dardziński <rafal.dardzinski@gmail.com>
2019-01-16 17:16:24 +0100

    added: tweened-number component

---
+++ src/components/tweened-number.vue
@@ -0,0 +1,47 @@
+<template>
+  <span class="tweened-number">
+    {{ number }}
+  </span>
+</template>
+<script>
+import TweenLite from 'gsap/TweenLite';
+
+export default {
+  props: {
+    value: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    decimal: {
+      type: Number,
+      default: 0
+    },
+    duration: {
+      type: Number,
+      default: 1.4,
+    },
+  },
+  computed: {
+    number() {
+      return this.tweenedValue.toFixed(this.decimal);
+    }
+  },
+  data() {
+    return {
+      tweenedValue: 0,
+    }
+  },
+  mounted() {
+    TweenLite.to(this, this.duration, { tweenedValue: this.value, ease: Power3.easeInOut })
+  },
+  watch: {
+    value() {
+      TweenLite.to(this, this.duration, { tweenedValue: this.value, ease: Power3.easeInOut })
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
