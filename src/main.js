834cd40a6d3c235293a4c2ba724974f9c3a7c3cc
Rafał Dardziński <rafal.dardzinski@gmail.com>
2019-01-16 11:11:52 +0100

    added: IE polyfills for fetch and promise

--- src/main.js
+++ src/main.js
@@ -3,9 +3,19 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import FontAwesomeIcon from '@/config/font-awesome';
+import * as Bluebird from 'bluebird';
+import { fetch as fetchPolyfill } from 'whatwg-fetch';
 
 Vue.config.productionTip = false;
 Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+if (!window.Promise) {
+  window.Promise = Bluebird;
+}
+
+if (!window.fetch) {
+  window.fetch = fetchPolyfill;
+}
 
 new Vue({
   router,
