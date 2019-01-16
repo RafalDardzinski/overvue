849cef8b94da5ea40085fac03fc53ab8086ae831
Rafał Dardziński <rafal.dardzinski@gmail.com>
2019-01-16 11:15:57 +0100

    fixed request module for proper error-handling

--- src/data-access-layer/utils/request.js
+++ src/data-access-layer/utils/request.js
@@ -1,15 +1,17 @@
 import encodeParameters from '@/data-access-layer/utils/encode-parameters';
 
-
 const request = (baseUrl, endpoint = '', params = {}) => {
   const queryString = encodeParameters(params);
   const url = `${baseUrl}/${endpoint}?${queryString}`;
   console.log(url);
   return fetch(url).then(res => {
-    const x = res.json();
-
-    if (res.ok) return res.json();
-    return Promise.reject(res.json())
+    if (!res.ok) {
+      const error = new Error(`Data fetch error. ${res.status}: ${res.statusText}`);
+      error.code = res.status;
+      error.res = res;
+      throw error;
+    }
+    return res.json();
   });
 };
 
